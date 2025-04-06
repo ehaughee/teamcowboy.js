237e24583e8ebdc2a619a64cc008953f2be28195
Eric Haughee <ehaughee@gmail.com>
2025-04-06 12:50:44 -0700

    feat: add Event_SaveRSVP support

--- src/methods/event.ts
+++ src/methods/event.ts
@@ -2,6 +2,32 @@ import { create } from "../request/params.js";
 import { TCResponse } from "../request/types.js";
 import { Client, TCEvent } from "../types.js";
 
+export type EventMethods = {
+  readonly get: Get;
+  readonly saveRSVP: SaveRSVP;
+};
+
+type SaveRSVPResponse = TCResponse<{
+  readonly success: boolean;
+  readonly errorCode?: "rsvpOverTotal" | "rsvpNotAllowed" | "userNotOnTeam" | "commentsOverMaxLength" | "generalError";
+}>;
+
+type SaveRSVPOptions = {
+  readonly teamId: number;
+  readonly eventId: number;
+  readonly status: "yes" | "maybe" | "available" | "no" | "noresponse";
+  readonly addlMale?: number | null;
+  readonly addlFemale?: number | null;
+  readonly comments?: string | null;
+  readonly rsvpAsUserId?: number | null;
+};
+
+type SaveRSVP = (options: SaveRSVPOptions) => Promise<SaveRSVPResponse>;
+
+type SaveRSVPRequestParams = SaveRSVPOptions & {
+  readonly method: "Event_SaveRSVP";
+};
+
 type GetResponse = TCResponse<TCEvent>;
 
 type GetOptions = {
@@ -9,14 +35,35 @@ type GetOptions = {
   readonly eventId: number;
   readonly includeRSVPInfo?: boolean | null;
 };
-type Get = (options: GetOptions) => Promise<GetResponse>;
 
-export type EventMethods = {
-  readonly get: Get;
-};
+type Get = (options: GetOptions) => Promise<GetResponse>;
 
 type GetEventRequestParams = GetOptions & {
   readonly method: "Event_Get";
+};
+
+const saveRSVP = async (
+  client: Client,
+  { teamId, eventId, status, addlMale, addlFemale, comments, rsvpAsUserId }: SaveRSVPOptions,
+) => {
+  const params = create<SaveRSVPRequestParams>({
+    client,
+    httpMethod: "POST",
+    params: {
+      method: "Event_SaveRSVP",
+      teamId,
+      eventId,
+      status,
+      addlMale: addlMale ?? null,
+      addlFemale: addlFemale ?? null,
+      comments: comments ?? null,
+      rsvpAsUserId: rsvpAsUserId ?? null,
+    },
+  });
+
+  const { data } = await client.axiosInstance.post<SaveRSVPResponse>("/", params);
+
+  return data;
 };
 
 const get = async (
@@ -42,5 +89,6 @@ const get = async (
 export const eventMethods = (client: Client): EventMethods => {
   return {
     get: (options) => get(client, options),
+    saveRSVP: (options) => saveRSVP(client, options),
   };
 };
